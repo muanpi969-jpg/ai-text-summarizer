b35ac4ed65af1a19ff3c223cd529db8dee4a9eb3
Pau Muan Tung HATLANG <muanpi969@gmail.com>
2026-04-01 03:58:33 +0530

    Add type hints and new utility functions
    Refactor clean_text function to include type hints and add count_words and timed functions.

--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 import re
+import time
 
-def clean_text(text):
-
-    text = re.sub(r'\s+', ' ', text)
-
-    text = re.sub(r'\[[^]]*\]', '', text)
-
+def clean_text(text: str) -> str:
+    text = re.sub(r"\s+", " ", text)
+    text = re.sub(r"\[[^\]]*\]", "", text)
     return text.strip()
+
+def count_words(text: str) -> int:
+    return len(text.split())
+
+def timed(func, *args, **kwargs):
+    start = time.time()
+    result = func(*args, **kwargs)
+    end = time.time()
+    return result, round(end - start, 3)
